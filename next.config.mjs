2df3cfa7e1726f02b501a6fd9c3462c33e78f873
Kv_Vedasri <154035170+Kv-Vedasri@users.noreply.github.com>
2026-02-06 07:27:06 +0530

    chore: initial project setup with Prisma, Tailwind CSS, and public assets

---
+++ next.config.mjs
@@ -0,0 +1,36 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverComponentsHmrCache: false,
+  },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "oohripqpyfnhjnkgtgqm.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/embed",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-src 'self' https://roadsidecoder.created.app;",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
